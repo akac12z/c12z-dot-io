f38093acd119c11986a60ac163fb299ddba40652
Chema <mrjark7@gmail.com>
2025-07-06 19:51:43 +0200

    feat: add SEO support for projects page with keywords and OpenGraph metadata

---
+++ src/components/SEO/pages/projects/projects.keywords.ts
@@ -0,0 +1,12 @@
+import type { PageKeywords } from "@interfaces/keywords.interface";
+
+export const projectsKeywords: PageKeywords = {
+  keywords: [
+    "projects",
+    "proyectos de chema ferrandez",
+    "proyectos de c12z",
+    "chema ferrandez",
+    "chema ferrandez projects",
+    "chema ferrandez proyectos",
+  ],
+};
