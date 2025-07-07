37f1fe8fa16c1842f6145ae7419f355071e37ff8
Chema <mrjark7@gmail.com>
2025-07-07 20:37:20 +0200

    feat: enhance SEO component by adding optional ogImage prop and updating image URLs for better social media sharing

---
+++ src/global/projectsInfo.ts
@@ -0,0 +1,7 @@
+import type { PagesInfo } from "@interfaces/pagesInfo.interface";
+
+export const LVM_es_Info: PagesInfo = {
+  title: "La vida Moderna es... by Chema Ferrandez",
+  description:
+    "Un homenaje a las frases y momentos de #Moderdonia, el programa que me marcó con humor, irreverencia y mucha cutrez.",
+};
