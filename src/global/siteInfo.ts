fa6c50829d019fe3c9ccae05299ff98b282bd1cb
Chema <mrjark7@gmail.com>
2025-12-31 17:08:34 +0100

    refactor: Relocate book SEO component, introduce generic content SEO, update global styling

--- src/global/siteInfo.ts
+++ src/global/siteInfo.ts
@@ -9,7 +9,7 @@ export const SITE_DEFAULT_CONFIG: SiteDefaultConfigInterface = {
   description:
     "Un 'building(me) in public' donde comparto aquello que voy haciendo, aprendiendo y pensando en mi carrera sobre Growth, Behavioral Dev y Product.",
   url: "https://c12z.io",
-  author: "Chema Ferrandez | c12z",
+  author: "Chema Ferrandez | cz",
   location: "es_ES",
   lang: "es-ES",
 };
@@ -19,7 +19,7 @@ export const SITE_404_CONFIG: Site404ConfigInterface = {
   errorDescription:
     "Ha habido un problema con la página que estabas buscando y bueno... aquí estamos.",
   url: "https://c12z.io",
-  author: "Chema Ferrandez | c12z",
+  author: "Chema Ferrandez | cz",
   location: "es_ES",
   lang: "es-ES",
 };
