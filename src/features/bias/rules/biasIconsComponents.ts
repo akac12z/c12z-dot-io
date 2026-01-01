3b0ca1a1d05371d869a1c371bd7b49a330cf5db8
Chema <mrjark7@gmail.com>
2026-01-01 16:01:50 +0100

    chaged bias and hevarior pages directories

---
+++ src/features/bias/rules/biasIconsComponents.ts
@@ -0,0 +1,16 @@
+// import type { BiasIcon } from "./biasIcons.interface";
+// import AnchorEffect from "../icons/AnchorEffect.icon.astro";
+// import BackfireEffect from "../icons/BackfireEffect.icon.astro";
+
+// export const allBiasIcon: BiasIcon[] = [
+//   {
+//     id: "anchor-effect",
+//     name: "efecto-ancla",
+//     icon: AnchorEffect,
+//   },
+//   {
+//     id: "backfire-effect",
+//     name: "efecto-backfire",
+//     icon: BackfireEffect,
+//   },
+// ];
