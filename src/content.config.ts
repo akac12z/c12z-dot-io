f3cc1f9c2313060edc4419a1afaeb91e99ea9c4c
Chema <mrjark7@gmail.com>
2026-01-07 08:55:35 +0100

    added isIndexed so as not to forget google indexing

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,205 +10,210 @@ import { isValidDateFormat } from "@utils/validatingDate";
  */
 
 const essayCollection = defineCollection({
-  loader: glob({
-    pattern: "**/*.{md,mdx}",
-    base: "./src/content/essay",
-  }),
-  schema: z.object({
-    title: z.string().max(60),
-    description: z.string().min(110).max(160),
-    // essayImage: z.object({
-    //   src: z.union([z.string().url(), z.string()]),
-    //   alt: z.string(),
-    // }),
-    // keywords: z.array(z.string()),
-    // publishDate: z.string().refine(isValidDateFormat, {
-    //   message: "The date must be in the format: YYYY-MM-DD. Make sure you have written it in the correct format.",
-    // }),
-    // lastTimeEdited: z.string().refine(
-    //   (val) => (val ? isValidDateFormat(val) : true), {
-    //   message: "The date must be in the format: YYYY-MM-DD. Make sure you have written it in the correct format.",
-    //   }).transform((val, ctx) => {
-    //     const publishDate = ctx;
-    //     return val ?? publishDate;
-    //   }).optional(),
-    // tags: z.array(z.string()),
-    // language: z.enum(["es"]),
-    // author: z.string().default("c12z"),
-    // authorLink: z.string(),
-    // readingTime: z.string(),
-    // categories: z.array(z.string()),
-    // status: z.boolean().default(true),
-    // canonicalURL: z.string().url(),
-  }),
+	loader: glob({
+		pattern: "**/*.{md,mdx}",
+		base: "./src/content/essay",
+	}),
+	schema: z.object({
+		title: z.string().max(60),
+		description: z.string().min(110).max(160),
+		// essayImage: z.object({
+		//   src: z.union([z.string().url(), z.string()]),
+		//   alt: z.string(),
+		// }),
+		// keywords: z.array(z.string()),
+		// publishDate: z.string().refine(isValidDateFormat, {
+		//   message: "The date must be in the format: YYYY-MM-DD. Make sure you have written it in the correct format.",
+		// }),
+		// lastTimeEdited: z.string().refine(
+		//   (val) => (val ? isValidDateFormat(val) : true), {
+		//   message: "The date must be in the format: YYYY-MM-DD. Make sure you have written it in the correct format.",
+		//   }).transform((val, ctx) => {
+		//     const publishDate = ctx;
+		//     return val ?? publishDate;
+		//   }).optional(),
+		// tags: z.array(z.string()),
+		// language: z.enum(["es"]),
+		// author: z.string().default("c12z"),
+		// authorLink: z.string(),
+		// readingTime: z.string(),
+		// categories: z.array(z.string()),
+		// status: z.boolean().default(true),
+		// canonicalURL: z.string().url(),
+		// isDraft: z.boolean().default(true).optional(),
+		// isIndexed: z.boolean().default(false),
+	}),
 });
 
 const libraryCollection = defineCollection({
-  loader: glob({
-    pattern: "**/*.{md,mdx}",
-    base: "./src/content/library",
-  }),
-  schema: ({ image }) =>
-    z.object({
-      title: z.string().max(60),
-      cover: z.object({
-        src: image(),
-        alt: z.string(),
-      }),
-      titleTag: z.string().max(60),
-      description: z.string().min(110).max(160),
-      abstract: z.string().min(250).max(410),
-      backlog: z.enum(["wip", "upload"]),
-      quote: z.string().max(150),
-      score: z
-        .number()
-        .min(1, {
-          message: "The minimum score value is 1",
-        })
-        .max(5, {
-          message: "The maximum score value is 5",
-        })
-        .int("The numbers must be integer"),
-      publishDate: z.string().refine(isValidDateFormat),
-      tags: z.array(z.string()).optional(),
-      language: z.enum(["es", "en"]),
-      authors: z.union([
-        z.object({
-          name: z.string(),
-          link: z
-            .string()
-            .url()
-            .refine(
-              (link) =>
-                link.startsWith("https://www.") || link.startsWith("https://"),
-              {
-                message:
-                  "The author's URL must start with 'https://www.' or 'https://'",
-              }
-            ),
-        }),
-        z.array(
-          z.object({
-            name: z.string(),
-            link: z
-              .string()
-              .url()
-              .refine(
-                (link) =>
-                  link.startsWith("https://www.") ||
-                  link.startsWith("https://"),
-                {
-                  message:
-                    "The author's URL must start with 'https://www.' or 'https://'",
-                }
-              ),
-          })
-        ),
-      ]),
-      readingTime: z.number().optional(),
-      keywords: z.array(z.string()),
-      authorLink: z.string().optional(),
-      isDraft: z.boolean().default(true).optional(),
-      buyItOnAmazon: z
-        .object({
-          spain: z
-            .string()
-            .url()
-            .refine(
-              (url) =>
-                url.startsWith("https://www.amazon.es") ||
-                url.startsWith("https://amazon.es"),
-              {
-                message:
-                  "The URL must start with 'https://www.amazon.es' or 'https://amazon.es'.",
-              }
-            ),
-          usa: z
-            .string()
-            .url()
-            .refine(
-              (url) =>
-                url.startsWith("https://www.amazon.com") ||
-                url.startsWith("https://amazon.com"),
-              {
-                message:
-                  "The URL must start with 'https://www.amazon.com  or 'https://amazon.com'.",
-              }
-            )
-            .optional(),
-        })
-        .optional(),
-    }),
+	loader: glob({
+		pattern: "**/*.{md,mdx}",
+		base: "./src/content/library",
+	}),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string().max(60),
+			cover: z.object({
+				src: image(),
+				alt: z.string(),
+			}),
+			titleTag: z.string().max(60),
+			description: z.string().min(110).max(160),
+			abstract: z.string().min(250).max(410),
+			backlog: z.enum(["wip", "upload"]),
+			quote: z.string().max(150),
+			score: z
+				.number()
+				.min(1, {
+					message: "The minimum score value is 1",
+				})
+				.max(5, {
+					message: "The maximum score value is 5",
+				})
+				.int("The numbers must be integer"),
+			publishDate: z.string().refine(isValidDateFormat),
+			tags: z.array(z.string()).optional(),
+			language: z.enum(["es", "en"]),
+			authors: z.union([
+				z.object({
+					name: z.string(),
+					link: z
+						.string()
+						.url()
+						.refine(
+							(link) =>
+								link.startsWith("https://www.") || link.startsWith("https://"),
+							{
+								message:
+									"The author's URL must start with 'https://www.' or 'https://'",
+							}
+						),
+				}),
+				z.array(
+					z.object({
+						name: z.string(),
+						link: z
+							.string()
+							.url()
+							.refine(
+								(link) =>
+									link.startsWith("https://www.") ||
+									link.startsWith("https://"),
+								{
+									message:
+										"The author's URL must start with 'https://www.' or 'https://'",
+								}
+							),
+					})
+				),
+			]),
+			readingTime: z.number().optional(),
+			keywords: z.array(z.string()),
+			authorLink: z.string().optional(),
+			isDraft: z.boolean().default(true).optional(),
+			isIndexed: z.boolean().default(false),
+			buyItOnAmazon: z
+				.object({
+					spain: z
+						.string()
+						.url()
+						.refine(
+							(url) =>
+								url.startsWith("https://www.amazon.es") ||
+								url.startsWith("https://amazon.es"),
+							{
+								message:
+									"The URL must start with 'https://www.amazon.es' or 'https://amazon.es'.",
+							}
+						),
+					usa: z
+						.string()
+						.url()
+						.refine(
+							(url) =>
+								url.startsWith("https://www.amazon.com") ||
+								url.startsWith("https://amazon.com"),
+							{
+								message:
+									"The URL must start with 'https://www.amazon.com  or 'https://amazon.com'.",
+							}
+						)
+						.optional(),
+				})
+				.optional(),
+		}),
 });
 
 const biasCollection = defineCollection({
-  // type: "content",
-  loader: glob({
-    pattern: "**/*.{md,mdx}",
-    base: "./src/content/bias",
-  }),
-  schema: ({ image }) =>
-    z
-      .object({
-        biasName: z.string().max(80),
-        biasID: z.string(),
-        cover: z.object({
-          src: image().optional(),
-          alt: z.string(),
-        }),
-        titleTag: z.string().max(60),
-        description: z.string().min(110).max(160),
-        biasQuestion: z.string().max(120),
-        backlog: z.enum(["wip", "upload"]),
-        publishDate: z.string().refine(isValidDateFormat),
-        lastTimeEdited: z
-          .string()
-          .refine(isValidDateFormat)
-          .refine((val) => (val ? isValidDateFormat(val) : true))
-          .transform((val, ctx) => {
-            const publishDate = ctx;
-            return val ?? publishDate;
-          })
-          .optional(),
-        tags: z.array(z.string()),
-        language: z.enum(["es"]),
-        keywords: z.array(z.string()),
-        relatedLinks: z
-          .array(
-            z.object({
-              label: z.string(),
-              url: z.string().url(),
-            })
-          )
-          .optional(),
-        readingTime: z.number().optional(),
-        category: z.array(
-          z.enum(["velocidad", "memoria", "percepción", "contexto", "juicio"])
-        ),
-      })
-      .refine(
-        (data) => {
-          if (data.lastTimeEdited && data.publishDate) {
-            const publishDateObj = new Date(data.publishDate);
-            const lastTimeEditedObj = new Date(data.lastTimeEdited);
-            return lastTimeEditedObj >= publishDateObj;
-          }
-          return true; // Si no hay lastTimeEdited, no aplica la validación en el frontmatter pero cuando se crea el componente, se le añade en el componente del {blog/bias/essay}SEO.astro para los metatags
-        },
-        {
-          message:
-            "The field { lastTimeEdited } cannot be earlier than { publishDate }.",
-          path: ["lastTimeEdited"], // Indica el campo donde se muestra el error
-        }
-      ),
+	// type: "content",
+	loader: glob({
+		pattern: "**/*.{md,mdx}",
+		base: "./src/content/bias",
+	}),
+	schema: ({ image }) =>
+		z
+			.object({
+				biasName: z.string().max(80),
+				biasID: z.string(),
+				cover: z.object({
+					src: image().optional(),
+					alt: z.string(),
+				}),
+				titleTag: z.string().max(60),
+				description: z.string().min(110).max(160),
+				biasQuestion: z.string().max(120),
+				backlog: z.enum(["wip", "upload"]),
+				publishDate: z.string().refine(isValidDateFormat),
+				lastTimeEdited: z
+					.string()
+					.refine(isValidDateFormat)
+					.refine((val) => (val ? isValidDateFormat(val) : true))
+					.transform((val, ctx) => {
+						const publishDate = ctx;
+						return val ?? publishDate;
+					})
+					.optional(),
+				tags: z.array(z.string()),
+				language: z.enum(["es"]),
+				keywords: z.array(z.string()),
+				isDraft: z.boolean().default(true).optional(),
+				isIndexed: z.boolean().default(false),
+				relatedLinks: z
+					.array(
+						z.object({
+							label: z.string(),
+							url: z.string().url(),
+						})
+					)
+					.optional(),
+				readingTime: z.number().optional(),
+				category: z.array(
+					z.enum(["velocidad", "memoria", "percepción", "contexto", "juicio"])
+				),
+			})
+			.refine(
+				(data) => {
+					if (data.lastTimeEdited && data.publishDate) {
+						const publishDateObj = new Date(data.publishDate);
+						const lastTimeEditedObj = new Date(data.lastTimeEdited);
+						return lastTimeEditedObj >= publishDateObj;
+					}
+					return true; // Si no hay lastTimeEdited, no aplica la validación en el frontmatter pero cuando se crea el componente, se le añade en el componente del {blog/bias/essay}SEO.astro para los metatags
+				},
+				{
+					message:
+						"The field { lastTimeEdited } cannot be earlier than { publishDate }.",
+					path: ["lastTimeEdited"], // Indica el campo donde se muestra el error
+				}
+			),
 });
 
 const projectCollections = defineCollection({
-  loader: glob({
-    pattern: "**/*.{md,mdx}",
-    base: "./src/content/projects",
-  }),
-  /*schema: ({ image }) =>
+	loader: glob({
+		pattern: "**/*.{md,mdx}",
+		base: "./src/content/projects",
+	}),
+	/*schema: ({ image }) =>
       z.object({
         project: z.string(),
         description: z.string().min(110).max(140),
@@ -219,12 +224,14 @@ const projectCollections = defineCollection({
         publishDate: z.string().refine(isValidDateFormat),
         keywords: z.array(z.string()),
         backlog: z.enum(["wip", "upload"]),
+        isDraft: z.boolean().default(true).optional(),
+				isIndexed: z.boolean().default(false),
       }),*/
 });
 
 export const collections = {
-  bias: biasCollection,
-  library: libraryCollection,
+	bias: biasCollection,
+	library: libraryCollection,
 };
 // essay: essayCollection,
 // projects: projectCollections
