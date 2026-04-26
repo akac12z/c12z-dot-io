aa2c9dd18f527f23f36249b2f92172425336fba7
Chema <mrjark7@gmail.com>
2026-04-26 21:05:52 +0200

    added site version

--- src/global/siteInfo.ts
+++ src/global/siteInfo.ts
@@ -1,24 +1,26 @@
 import type {
-  Site404ConfigInterface,
-  SiteDefaultConfigInterface,
+	Site404ConfigInterface,
+	SiteDefaultConfigInterface,
 } from "@interfaces/siteInfo.interface";
 
+export const SITE_VERSION = "v1.2.0";
+
 export const SITE_DEFAULT_CONFIG: SiteDefaultConfigInterface = {
-  title: "Chema Ferrandez - c12z",
-  description:
-    "Un 'building(me) in public' donde comparto aquello que voy haciendo, aprendiendo y pensando en mi carrera sobre Growth, Behavioral Dev y Product.",
-  url: "https://c12z.io",
-  author: "Chema Ferrandez | cz",
-  location: "es_ES",
-  lang: "es-ES",
+	title: "Chema Ferrandez - c12z",
+	description:
+		"Un 'building(me) in public' donde comparto aquello que voy haciendo, aprendiendo y pensando en mi carrera sobre Growth, Behavioral Dev y Product.",
+	url: "https://c12z.io",
+	author: "Chema Ferrandez | cz",
+	location: "es_ES",
+	lang: "es-ES",
 };
 
 export const SITE_404_CONFIG: Site404ConfigInterface = {
-  errorTitle: "Houston, tenemos un error 404",
-  errorDescription:
-    "Ha habido un problema con la página que estabas buscando y bueno... aquí estamos.",
-  url: "https://c12z.io",
-  author: "Chema Ferrandez | cz",
-  location: "es_ES",
-  lang: "es-ES",
+	errorTitle: "Houston, tenemos un error 404",
+	errorDescription:
+		"Ha habido un problema con la página que estabas buscando y bueno... aquí estamos.",
+	url: "https://c12z.io",
+	author: "Chema Ferrandez | cz",
+	location: "es_ES",
+	lang: "es-ES",
 };
