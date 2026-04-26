728cf066a48d2d779a9cb110658504bc3e2beffe
Chema <mrjark7@gmail.com>
2026-04-26 18:58:10 +0200

    new sections ui

--- src/features/home/rules/sectionLists.ts
+++ src/features/home/rules/sectionLists.ts
@@ -1,38 +1,37 @@
 import type { Section } from "./sectionLists.interface";
 
 export const SECTION_LISTS: Section[] = [
-  {
-    href: "/proyectos",
-    style: "proyectos",
-    label: "proyectos",
-    description: "Cosas que voy creando y compartiendo",
-    items: [
-      {
-        text: "#LaVidaModerna es...",
-        href: "/proyectos/la-vida-moderna-es",
-      },
-    ],
-  },
-  {
-    href: "/biblioteca",
-    style: "biblioteca",
-    label: "biblioteca",
-    description: "Notas de los libros que voy leyendo",
-    items: [],
-  },
-  {
-    href: "/ensayos",
-    style: "ensayos",
-    label: "ensayos",
-    description: "Insights, frameworks y pensamientos sobre Growth y Producto",
-    items: [],
-  },
-  {
-    href: "/behavior",
-    style: "behavior",
-    label: "behavior",
-    description:
-      "Cómo y por qué hacemos lo que hacemos y cómo aplicarlo a Producto",
-    items: [],
-  },
+	{
+		href: "/proyectos",
+		style: "proyectos",
+		label: "proyectos",
+		description: "Cosas que voy creando y compartiendo",
+		items: [
+			{
+				text: "#LaVidaModerna es...",
+				href: "/proyectos/la-vida-moderna-es",
+			},
+		],
+	},
+	{
+		href: "/biblioteca",
+		style: "biblioteca",
+		label: "biblioteca",
+		description: "Notas de los libros que voy leyendo",
+		items: [],
+	},
+	{
+		href: "/ensayos",
+		style: "ensayos",
+		label: "ensayos",
+		description: "Frameworks y pensamientos sobre Growth y Producto",
+		items: [],
+	},
+	{
+		href: "/behavior",
+		style: "behavior",
+		label: "behavior",
+		description: "Cómo y por qué hacemos lo que hacemos",
+		items: [],
+	},
 ];
