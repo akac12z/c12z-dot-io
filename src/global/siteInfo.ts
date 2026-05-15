35d77506749a6a50bbec48d76c43921731a6ced9
Chema <mrjark7@gmail.com>
2026-05-15 11:52:12 +0200

    fixed inc views

--- src/global/siteInfo.ts
+++ src/global/siteInfo.ts
@@ -3,7 +3,7 @@ import type {
 	SiteDefaultConfigInterface,
 } from "@interfaces/siteInfo.interface";
 
-export const SITE_VERSION = "v1.6.12";
+export const SITE_VERSION = "v1.6.13";
 
 export const SITE_DEFAULT_CONFIG: SiteDefaultConfigInterface = {
 	title: "Chema Ferrandez - c12z",
